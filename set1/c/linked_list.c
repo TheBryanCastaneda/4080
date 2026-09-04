589477b5c892437a2972cc5ede1551af11577fdd
TheBryanCastaneda <bryanccastaneda0@gmail.com>
2026-09-04 00:23:39 -0700

    Update linked_list.c
    halp

--- set1/c/linked_list.c
+++ set1/c/linked_list.c
@@ -10,4 +10,48 @@ typedef struct Node {
     
 } Node;
 
-static char *
+static char *copy_string(const char *str) {
+    size_t lenght = strlen(value) + 1;
+    char *copy = malloc(lenght);
+
+    if (copy == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    memcpy(copy, value, lenght);
+    return copy;
+}
+
+Node *insert(Node **head, Node *after, const char *value) {
+    Node *node = malloc(sizeof(*node));
+
+    if (node == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    node->value = copy_string(value);
+    node->prev = NULL;
+    node->next = *head;
+
+    if(*head != NULL) {
+        (*head)->prev = node;
+    }
+
+    *head = node;
+
+} else {
+    node->prev = after;
+    node->next = after->next;
+
+    if (after->next != NULL) {
+        after->next->prev = node;
+    }
+
+    after->next = node;
+}
+
+return node;
+}
+
